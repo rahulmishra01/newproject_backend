5e8ce0473b0af66980161ab9726988b78d7a5d8b
rahulmishra01 <rahulmishra3865@gmail.com>
2024-05-22 11:21:27 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", (data) => {
+    console.log(`Incoming call from ${data.from}`);
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+
+  socket.on("answerCall", (data) => {
+    console.log(`Answering call from ${data.from}`);
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+server.listen(5000, () => console.log("server is running on port 5000"));
