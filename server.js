748f4d0488bbaa37cdf8b3c70050c98b7f25ff9c
rahulmishra01 <rahulmishra3865@gmail.com>
2024-05-22 12:23:40 +0530

    added the changes

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-const cors = require("cors");
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-
-app.use(cors());
-app.use(
-  cors({
-    origin: "https://newproject-frontend.onrender.com",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-io.on("connection", (socket) => {
-  console.log("New user connected");
-
-  socket.on("callUser", (data) => {
-    io.to(data.to).emit("ring", { from: data.from });
+const io = socketIo(server, {
+    cors: {
+      origin: "https://new-omagle.onrender.com",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    },
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('callUser', (data) => {
+    io.to(data.to).emit('ring', { from: data.from });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
   });
 });
 
 server.listen(5000, () => {
-  console.log("Server is running on port 5000");
+  console.log('Server is running on port 5000');
 });
