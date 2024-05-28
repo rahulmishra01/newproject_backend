f16e637d29198b2b83f8535c65baf5b9b5130fa8
rahulmishra01 <rahulmishra3865@gmail.com>
2024-05-28 12:16:38 +0530

    new changes

--- server.js
+++ server.js
@@ -1,42 +1,3 @@
-// const express = require("express");
-// const http = require("http");
-// const { Server } = require("socket.io");
-// const cors = require("cors");
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   },
-// });
-
-// app.use(cors());
-
-// io.on("connection", (socket) => {
-//   console.log("A user connected:", socket.id);
-
-//   socket.on("callUser", ({ from, to }) => {
-//     io.to(to).emit("ring", { from });
-//   });
-
-//   socket.on("answerCall", ({ to, signal }) => {
-//     io.to(to).emit("callAccepted", signal);
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log("A user disconnected:", socket.id);
-//   });
-// });
-
-// const PORT = process.env.PORT || 5000;
-// server.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -54,25 +15,19 @@ const io = new Server(server, {
 
 app.use(cors());
 
-let waitingUser = null;
-
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
-  if (waitingUser) {
-    // Pair the current user with the waiting user
-    io.to(socket.id).emit("pair", { partnerId: waitingUser });
-    io.to(waitingUser).emit("pair", { partnerId: socket.id });
-    waitingUser = null;
-  } else {
-    waitingUser = socket.id;
-  }
+  socket.on("callUser", ({ from, to }) => {
+    io.to(to).emit("ring", { from });
+  });
+
+  socket.on("answerCall", ({ to, signal }) => {
+    io.to(to).emit("callAccepted", signal);
+  });
 
   socket.on("disconnect", () => {
     console.log("A user disconnected:", socket.id);
-    if (waitingUser === socket.id) {
-      waitingUser = null;
-    }
   });
 });
 
