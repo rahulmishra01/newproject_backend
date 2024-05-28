9243e39e9b1d652b42bfe3b3de913352d4caaaaf
rahulmishra01 <rahulmishra3865@gmail.com>
2024-05-28 14:47:43 +0530

    here is my old changes

--- server.js
+++ server.js
@@ -1,44 +1,3 @@
-// const express = require("express");
-// const http = require("http");
-// const { Server } = require("socket.io");
-// const cors = require("cors");
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: "https://newproject-frontend.onrender.com",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   },
-// });
-
-// app.use(cors());
-
-// io.on("connection", (socket) => {
-//   console.log("A user connected:", socket.id);
-
-//   socket.on("callUser", ({ from, to }) => {
-//     io.to(to).emit("ring", { from });
-//   });
-
-//   socket.on("answerCall", ({ to, signal }) => {
-//     io.to(to).emit("callAccepted", signal);
-//   });
-//   socket.on("endCall", ({ to }) => {
-//     io.to(to).emit("callEnded");
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log("A user disconnected:", socket.id);
-//   });
-// });
-
-// const PORT = process.env.PORT || 5000;
-// server.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -56,20 +15,8 @@ const io = new Server(server, {
 
 app.use(cors());
 
-let waitingUsers = [];
-
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
-
-  socket.on("findMatch", () => {
-    if (waitingUsers.length > 0) {
-      const match = waitingUsers.pop();
-      io.to(match).emit("matchFound", { userId: socket.id });
-      socket.emit("matchFound", { userId: match });
-    } else {
-      waitingUsers.push(socket.id);
-    }
-  });
 
   socket.on("callUser", ({ from, to }) => {
     io.to(to).emit("ring", { from });
@@ -78,14 +25,12 @@ io.on("connection", (socket) => {
   socket.on("answerCall", ({ to, signal }) => {
     io.to(to).emit("callAccepted", signal);
   });
-
   socket.on("endCall", ({ to }) => {
     io.to(to).emit("callEnded");
   });
 
   socket.on("disconnect", () => {
     console.log("A user disconnected:", socket.id);
-    waitingUsers = waitingUsers.filter(user => user !== socket.id);
   });
 });
 
