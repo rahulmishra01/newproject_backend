8a6a0e6768b277a30ff7bd2317570ee28e967f90
rahulmishra01 <rahulmishra3865@gmail.com>
2024-06-11 14:29:39 +0530

    changes

--- server.js
+++ server.js
@@ -15,8 +15,19 @@ const io = new Server(server, {
 
 app.use(cors());
 
+let waitingQueue = [];
+
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
+  socket.on("joinRandomChat", () => {
+    if (waitingQueue.length > 0) {
+      const peerId = waitingQueue.pop();
+      io.to(socket.id).emit("peerFound", peerId);
+      io.to(peerId).emit("peerFound", socket.id);
+    } else {
+      waitingQueue.push(socket.id);
+    }
+  });
 
   socket.on("callUser", ({ from, to }) => {
     io.to(to).emit("ring", { from });
@@ -31,6 +42,7 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     console.log("A user disconnected:", socket.id);
+    waitingQueue = waitingQueue.filter(id => id !== socket.id);
   });
 });
 
